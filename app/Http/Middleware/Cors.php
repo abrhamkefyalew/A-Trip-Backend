499c8e5e3b23b50ea1f786bbfff839d4570ba9cc
abrhamkefyalew <abrekefe22@gmail.com>
2024-08-18 21:40:25 +0300

    trying to fix cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+{
+    $response = $next($request);
+
+    $response->header('Access-Control-Allow-Origin', '*');
+    $response->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET, OPTIONS');
+    $response->header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type');
+
+    return $response;
+}
+}
