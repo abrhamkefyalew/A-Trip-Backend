058cd85999c3413ac2adf3daff2ef5773f13cee1
abrhamkefyalew <abrekefe22@gmail.com>
2024-08-18 22:08:42 +0300

    four

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,14 +15,14 @@ class Cors
      * @param  \Closure  $next
      * @return \Illuminate\Http\Response
      */
-    public function handle(Request $request, Closure $next): Response
-{
-    $response = $next($request);
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
 
-    $response->header('Access-Control-Allow-Origin', '*');
-    $response->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET, OPTIONS');
-    $response->header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type');
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type', 'X-Requested-With');
 
-    return $response;
-}
+        return $response;
+    }
 }
